c7d4c6e4a0ad9a7a90951404867966565c0abda6
trijal452009 <89978519+trijal452009@users.noreply.github.com>
2023-01-16 11:04:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var first_name ="Trijal"; 
+var last_name="Sharma";
+var a = 10; var b = 5;
+var add = a + b ; 
+console.log(add);
+var concat= concat(first_name + last_name);
+console.log(concat);
+console.log("first number is ", a);
+console.log("second number is ", b);
